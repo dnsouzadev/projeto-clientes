9ba45dff0691048ce9549dc97ffeca5e1c3a977c
Daniel Souza <workdndsza@gmail.com>
2024-06-24 13:10:47 -0300

    validando o celular, rg e o nome

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -10,3 +10,18 @@ class ClienteSerializer(serializers.ModelSerializer):
         if len(cpf) != 11:
             raise serializers.ValidationError('O CPF deve ter 11 dígitos')
         return cpf
+
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError('O RG deve ter 9 dígitos')
+        return rg
+
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError('O celular deve ter no mínimo 11 dígitos')
+        return celular
+
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('O nome deve conter apenas letras')
+        return nome
